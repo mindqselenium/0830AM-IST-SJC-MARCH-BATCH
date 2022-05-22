834c4d3665026121019f3931216a7453af1a075a
user <user@DESKTOP-JER5TCF>
2022-05-22 15:02:23 +0530

    new homepage test case

---
+++ OHRMS/src/test/java/testcasescripts/HomePageTest.java
@@ -0,0 +1,18 @@
+package testcasescripts;
+
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+import com.ohrms.baseclass.Baseclass;
+
+public class HomePageTest extends Baseclass {
+  public HomePageTest() throws IOException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+@Test
+  public void f() {
+  }
+}
